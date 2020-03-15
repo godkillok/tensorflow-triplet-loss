e19417a383728f9c7faf1be002b338827995e417
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2020-03-15 16:40:08 +0800

    12

--- model/common_tool.py
+++ model/common_tool.py
@@ -11,15 +11,28 @@ def clean_str(text):
     return text
 
 def per_line(line):
-    li = json.loads(line)
-    if  isinstance(li.get('pic_tags', ''),str):
-        pic_tags = [pic.split('\x02')[0] for pic in li.get('pic_tags', '').split('\x03')]
-    elif isinstance(li.get('pic_tags', ''),list):
-        pic_tags =li.get('pic_tags', '')
-    else:
-        pic_tags =[""]
-    fields = li.get("title", "") + ' ' + ' '.join(li.get("tags", [""])) + ' '.join(pic_tags)
-    text = clean_str(fields.lower())
-    text = text.replace("\n", ' ').replace("\r", " ")
-    return text
+    li = line.split("__label__")
+    labels=[]
+    for l in li[1:]:
+        punctuation = r"""0123456789"""
+        text = re.sub(r'[{}]+'.format(punctuation), ' ', str(l))
+        text = ' '.join(text.split())
+        text = text.lower()
+        text = ' '.join(text.split("\x01_"))
+        text = ' '.join(text.split())
+        labels.append(text)
+
+    punctuation = r"""!"#$%&()*+,-./:;<=>?@[\]^_`{|}~。，"""
+    text = line.split("__label__")[0].strip()
+    text = re.sub(r'[{}]+'.format(punctuation), ' ', str(text))
+    text = ' '.join(text.split())
+    text = re.sub(
+        '[.com\u200b\001\002\003\004\005\006\007\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a]+',
+        '', text)
+    #         print(str)
+    tokens = text.lower()
+    # print(tokens)
+    tokens = tokens.split()
+    tokens = [w.strip() for w in tokens if len(w.strip()) > 0 and not w.isdigit()]
+    return tokens,labels
 #1
