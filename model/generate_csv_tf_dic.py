0fc81086b5d95639f3a0ec85f7f6119671d18d61
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2020-03-15 23:30:52 +0800

    12

--- model/generate_csv_tf_dic.py
+++ model/generate_csv_tf_dic.py
@@ -173,7 +173,7 @@ def write_tfrecords(tf_lines, path_text, count):
 
 def main():
     vocab_dict, label_dict= ini()
-    generate_tf_dic(os.path.join(FLAGS.data_dir, 'tag_space2'),vocab_dict,label_dict)
+    generate_tf_dic(os.path.join(FLAGS.data_dir, 'tag_space'),vocab_dict,label_dict)
     # generate_tf_dic(os.path.join(FLAGS.data_dir, 'tag_space2'),vocab_dict,label_dict)
     # generate_tf_dic(os.path.join(FLAGS.data_dir, 'txt_valid'), vocab_dict,label_dict)
     # s3_input = FLAGS.data_dir
