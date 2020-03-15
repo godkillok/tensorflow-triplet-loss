2749569151cb25d81b128703b7dd100db8fdc4b1
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2020-03-15 19:56:00 +0800

    12

--- model/generate_csv_tf_dic.py
+++ model/generate_csv_tf_dic.py
@@ -57,14 +57,14 @@ def parse_line_dict(record,vocab_dict,label_dict):
     if len(labels) >= 12:
         labels = labels[0: 12]
     else:
-        labels += ['-1'] * (12 - len(labels))
+        labels += ['-111'] * (12 - len(labels))
     tags=[]
     for lab in labels:
         tag=[]
         for la in lab.split(' '):
             # if la not in vocab_dict:
             #     print("'{}' not exist".format(la))
-            tag.append(vocab_dict.get(la,OOV))
+            tag.append(vocab_dict.get(la,0))
         tags.append(tag)
     labels=[label_dict.get(lab,-1) for lab in labels]
     print([text,labels,tags])
