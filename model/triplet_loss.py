a0f6a95a44afce2296b41baafc6bbc104e432acd
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2020-03-17 01:27:50 +0800

    12

--- model/triplet_loss.py
+++ model/triplet_loss.py
@@ -24,26 +24,27 @@ def _pairwise_distances(embeddings_a,embedding_b, squared=False):
     # Get squared L2 norm for each embedding. We can just take the diagonal of `dot_product`.
     # This also provides more numerical stability (the diagonal of the result will be exactly 0).
     # shape (batch_size,)
-    square_norm = tf.diag_part(dot_product)
-
-    # Compute the pairwise distance matrix as we have:
-    # ||a - b||^2 = ||a||^2  - 2 <a, b> + ||b||^2
-    # shape (batch_size, batch_size)
-    distances = tf.expand_dims(square_norm, 1) - 2.0 * dot_product + tf.expand_dims(square_norm, 0)
-
-    # Because of computation errors, some distances might be negative so we put everything >= 0.0
-    distances = tf.maximum(distances, 0.0)
-    tf.summary.histogram('distances', distances)
-    if not squared:
-        # Because the gradient of sqrt is infinite when distances == 0.0 (ex: on the diagonal)
-        # we need to add a small epsilon where distances == 0.0
-        mask = tf.to_float(tf.equal(distances, 0.0))
-        distances = distances + mask * 1e-16
-
-        distances = tf.sqrt(distances)
-
-        # Correct the epsilon added: set the distances on the mask to be exactly 0.0
-        distances = distances * (1.0 - mask)
+    # square_norm = tf.diag_part(dot_product)
+    #
+    # # Compute the pairwise distance matrix as we have:
+    # # ||a - b||^2 = ||a||^2  - 2 <a, b> + ||b||^2
+    # # shape (batch_size, batch_size)
+    # distances = tf.expand_dims(square_norm, 1) - 2.0 * dot_product + tf.expand_dims(square_norm, 0)
+    #
+    # # Because of computation errors, some distances might be negative so we put everything >= 0.0
+    # distances = tf.maximum(distances, 0.0)
+    # tf.summary.histogram('distances', distances)
+    # if not squared:
+    #     # Because the gradient of sqrt is infinite when distances == 0.0 (ex: on the diagonal)
+    #     # we need to add a small epsilon where distances == 0.0
+    #     mask = tf.to_float(tf.equal(distances, 0.0))
+    #     distances = distances + mask * 1e-16
+    #
+    #     distances = tf.sqrt(distances)
+    #
+    #     # Correct the epsilon added: set the distances on the mask to be exactly 0.0
+    #     distances = distances * (1.0 - mask)
+    distances =1- tf.maximum(dot_product, 0.0)
     return distances
 
 
