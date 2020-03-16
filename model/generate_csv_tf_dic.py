5d0cce6e3aa02e83895fa1004e218284c29e0ae8
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2020-03-16 08:47:34 +0800

    12

--- model/generate_csv_tf_dic.py
+++ model/generate_csv_tf_dic.py
@@ -55,7 +55,7 @@ def per_thouds_lines_dict(result_lines, path_text, count,pad_word,flag_name=''):
 
 
     for rl_num,rl in enumerate(result_lines):
-        g=line_para(rl)
+        g=line_para(rl,pad_word)
         tf_lines.append(g)
         # if rl_num>1 and rl_num%10000==0:
         #     flag_name=str(rl_num)
