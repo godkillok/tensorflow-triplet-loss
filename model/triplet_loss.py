076967696c321d04a7ae2eeb8fb01c0bf61e0efc
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2020-03-16 17:18:10 +0800

    12

--- model/triplet_loss.py
+++ model/triplet_loss.py
@@ -169,7 +169,7 @@ def batch_all_triplet_loss(labels, embeddings_a,embedding_b, margin, squared=Fal
     # Get final mean triplet loss over the positive valid triplets
     triplet_loss = tf.reduce_sum(triplet_loss) / (num_positive_triplets + 1e-16)
 
-    return triplet_loss, fraction_positive_triplets,num_positive_triplets,triplet_loss2,mask
+    return triplet_loss, fraction_positive_triplets,num_positive_triplets
 
 
 def batch_hard_triplet_loss(labels, embeddings_a,embedding_b, margin, squared=False):
