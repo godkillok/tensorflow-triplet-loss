6ce1dd21604be18374f6e5ae0a37e9eb3c730534
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2020-03-18 00:42:05 +0800

    12

--- model/triplet_loss.py
+++ model/triplet_loss.py
@@ -178,7 +178,9 @@ def batch_all_triplet_loss(labels, embeddings_a,embedding_b, margin, squared=Fal
     triplet_loss1=-tf.expand_dims(square_norm, 1) +pairwise_dist+0.2
     neg=pairwise_dist - tf.matrix_diag(square_norm)
     triplet_loss1 = tf.maximum(triplet_loss1, 0.0)
-    triplet_loss1=tf.reduce_max(triplet_loss1- tf.matrix_diag(square_norm),axis=1)
+
+    triplet_loss1=tf.reduce_max(triplet_loss1,axis=1)
+
     valid_triplets1 = tf.to_float(tf.greater(triplet_loss1, 1e-16))
     num_positive_triplets1 = tf.reduce_sum(valid_triplets1)
     triplet_loss=tf.reduce_sum(triplet_loss1) / (num_positive_triplets1 + 1e-16)
