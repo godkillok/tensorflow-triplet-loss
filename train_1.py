54f83e9ec120e076ff2c6c2c88d60ca16daed3c8
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2020-03-17 23:38:26 +0800

    12

--- train_1.py
+++ train_1.py
@@ -12,7 +12,7 @@ from model.utils import Params
 
 os.environ['CUDA_VISIBLE_DEVICES'] ="6,7,8"
 parser = argparse.ArgumentParser()
-parser.add_argument('--model_dir', default='/data/tanggp/tmp/tensorflow-triplet-loss/experiments/batch_all',
+parser.add_argument('--model_dir', default='/data/tanggp/tmp/tensorflow-triplet-loss/experiments/batch_all_2',
                     help="Experiment directory containing params.json")
 parser.add_argument('--data_dir', default='/data/tanggp/tmp/Starspace/python/test/*.tfrecords',
                     help="Directory containing the dataset")
