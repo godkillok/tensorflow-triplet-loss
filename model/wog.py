64fdc89546c6265d39e18a1e3bfce0665a92fa38
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2020-03-20 00:11:17 +0800

    12

---
+++ model/wog.py
@@ -0,0 +1,41 @@
+import re
+def per_line(line):
+    li = line.split("__label__")
+    labels=[]
+    for l in li[1:]:
+        punctuation = r"""0123456789"""
+        text = re.sub(r'[{}]+'.format(punctuation), ' ', str(l))
+        text = ' '.join(text.split())
+        text = text.lower()
+        text = ' '.join(text.split("\x01_"))
+        text = ' '.join(text.split())
+        labels.append(text)
+
+    punctuation = r"""!"#$%&()*+,-./:;<=>?@[\]^_`{|}~。，"""
+    text = line.split("__label__")[0].strip()
+    text = re.sub(r'[{}]+'.format(punctuation), ' ', str(text))
+    text = ' '.join(text.split())
+    text = re.sub(
+        '[.com\u200b\001\002\003\004\005\006\007\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a]+',
+        '', text)
+    #         print(str)
+    tokens = text.lower()
+    # print(tokens)
+    tokens = tokens.split()
+    tokens = [w.strip() for w in tokens if len(w.strip()) > 0 and not w.isdigit()]
+    return tokens,labels
+
+with open("/data/tanggp/tmp/Starspace/python/test/tag_space","r",encoding="utf8") as f:
+    lines=f.readlines()
+
+results=[]
+for li in lines:
+    tokens, labels=per_line(li.strip())
+    results.append(" ".join(tokens))
+    results+=labels
+
+with open("/data/tanggp/tmp/Starspace/python/test/tag_space_token","w",encoding="utf8") as f:
+    for res in results:
+        f.writelines(res+'\n')
+
+
